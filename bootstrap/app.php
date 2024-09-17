428ed41ec1b934aeb4ff8bade3a7795787b75ada
loireman <akarvasarskij@gmail.com>
2024-09-17 21:09:38 +0200

    cors 4

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\Cors;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -15,7 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
-
+        $middleware->append(Cors::class);
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
