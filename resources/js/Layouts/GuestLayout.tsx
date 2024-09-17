f4576afa3f61f21cd8dd65ff3fb78e29271cd6fc
loireman <akarvasarskij@gmail.com>
2024-09-17 18:46:49 +0200

    updated dashboard

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -4,14 +4,15 @@ import { PropsWithChildren } from 'react';
 
 export default function Guest({ children }: PropsWithChildren) {
     return (
-        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-50 text-black/50 dark:bg-neutral-950 dark:text-white/50">
             <div>
                 <Link href="/">
                     <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
                 </Link>
             </div>
 
-            <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            <div className="w-full sm:max-w-md mt-6 px-6 py-4 overflow-hidden sm:rounded-lg
+            grid items-start gap-10 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">
                 {children}
             </div>
         </div>
--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -8,9 +8,14 @@ export default function Login() {
         <GuestLayout>
             <Head title="Log in" />
 
-            <div className="p-4 text-xl dark:text-gray-200 text-center">Увійти до системи контролю фінансів</div>
+            <div className="p-4 text-xl dark:text-gray-200 text-center">
+                Увійти до системи контролю фінансів
+            </div>
 
-            <a href={route("auth.socialite.redirect")} className="flex gap-2 w-full justify-center items-center bg-gray-300 py-4 text-lg rounded-2xl">
+            <a
+                href={route("auth.socialite.redirect")}
+                className="flex gap-2 w-full justify-center items-center bg-neutral-600 hover:bg-neutral-700 py-4 px-4 rounded-lg font-semibold text-gray-300"
+            >
                 Log in with{" "}
                 <Icon icon="logos:google-icon" width={24} height={24} />
             </a>
