2f58f45f12658715f6d7d3b411c45aae4b543387
loireman <akarvasarskij@gmail.com>
2024-09-17 20:58:24 +0200

    cors update 2

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'http://127.0.0.1:11434/*'],
 
     'allowed_methods' => ['*'],
 
